11f85353a19109661b7641584a14582087b52c0d
David <david_knotten@hotmail.no>
2021-08-29 21:48:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/fantapp/FrontPageActivity.kt
@@ -0,0 +1,11 @@
+package com.davidrk.fantapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FrontPageActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.front_page)
+    }
+}
