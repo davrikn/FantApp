0fde029df42988ee0a375d6b4376a23725c1758b
David Rise Knotten <david_knotten@hotmail.no>
2021-08-30 11:40:05 +0200

    Work at school

--- app/src/main/java/com/example/fantapp/FrontPageActivity.kt
+++ app/src/main/java/com/example/fantapp/FrontPageActivity.kt
@@ -1,11 +1,19 @@
 package com.example.fantapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class FrontPageActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.front_page)
+
+        val label = findViewById<TextView>(R.id.fpUsernameText)
+        label.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
